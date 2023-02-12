24929cf4b31670d1d72f1f822fad8db219770e9c
AJorge18 <eduxd152@gmail.com>
2023-02-11 22:06:20 -0500

    Tarea 2

---
+++ Tarea 2/index.js
@@ -0,0 +1,16 @@
+const Info = [
+    {
+      name: "Jorge Aguirre",
+      age: 24,
+      developer: true,
+      birthday: new Date("1998-12-31"),
+      favoriteBook: {
+        title: "Don Quijote de la Mancha",
+        author: "Miguel de Cervantes",
+        date: new Date("1605-01-01"),
+        url: "https://es.wikipedia.org/wiki/Don_Quijote_de_la_Mancha"
+      }
+    }
+  ];
+
+  console.log(Info);
