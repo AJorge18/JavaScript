f886ec220156ec3a07d351bc8238249a3cab225e
AJorge18 <eduxd152@gmail.com>
2023-02-14 18:26:57 -0500

    Tarea 3

---
+++ estructuras-de-control/break-continue.js
@@ -0,0 +1,21 @@
+// Casos muy especificos - break, continue
+let lista=[1,2,3,4,5,6,7,8];
+
+for(let i=0;i<lista.length; i++){
+    if(lista[i]===3){
+        continue;
+    }
+
+    let j=5;
+    const k=100;
+
+    console.log(lista[i]);
+    if(lista[i]>5){
+        break;
+    }
+}
+
+//Cual es el ambito de un bucle
+console.log(i);
+console.log(j);
+console.log(k);
