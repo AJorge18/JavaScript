c6e9c0b4118239addc2786384d7c6a8d1121432b
AJorge18 <eduxd152@gmail.com>
2023-02-18 18:10:13 -0500

    Tarea 7

---
+++ Tarea 7/conjuntos.js
@@ -0,0 +1,11 @@
+// Set de nombres de mi familia
+let nombresFamilia = new Set(['Juan', 'Maria', 'Luisa', 'Pedro', 'Ana']);
+console.log(nombresFamilia);
+
+// Agregar mi nombre al Set (duplicado)
+nombresFamilia.add('Juan');
+console.log(nombresFamilia);
+
+// Agregar "Javascript" al Set
+nombresFamilia.add('Javascript');
+console.log(nombresFamilia);
